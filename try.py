01258bf7611489ecb9f7314fe5ee22a4df48d0a1
absorto <absorto@sdf.org>
2015-05-24 00:40:17 -0500

    plot erdos graph when nodes aren't in the same axis

--- try.py
+++ try.py
@@ -6,7 +6,7 @@ from itertools import combinations
 h = Hiveplot( 'aguas.svg')
 
 the_axes = [
-    Axis( (200,200), (300,100)),
+    Axis( (200,200), (200,100)),
     Axis( (200,200), (300,300)),
     Axis( (200,200), (10,310)) ]
 
@@ -26,10 +26,22 @@ for a in combinations(the_axes, 2):
     a0 = a[0]
     a1 = a[1]
 
+    if a0.end == (200,100) and a1.end == (300,300):
+        sa = 15
+        ta = -15
+    if a0.end == (200,100) and a1.end == (10,310):
+        sa = -15
+        ta = 15
+    if a0.end == (300,300) and a1.end == (10,310):
+        sa = 15
+        ta = -15
+
+        
     for e in g.edges():
         if (e[0] in a0.nodes) and (e[1] in a1.nodes):
-            h.connect(a0, e[0], 15,
-                      a1, e[1], 15)
+            h.connect(a0, e[0], sa,
+                      a1, e[1], ta,
+                      random.choice(['red','green','purple','blue','orange']))
     
 
 h.save()
