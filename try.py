f7e2fbd33577b01cceec49e89741aca4c94215c3
Rodrigo Garcia <absorto@sdf.org>
2015-05-25 18:06:05 -0500

    exemplify svg shape as node, passing attrs to edge

--- try.py
+++ try.py
@@ -17,15 +17,31 @@ h.axes.append( axis2 )
 # a random network
 g = nx.erdos_renyi_graph(100,0.3)
 
-# distribute nodes in axes
+# randomly distribute nodes in axes
 the_axes = [ axis0,
              axis1,
              axis2, ]
 for n in g.nodes():
-    n = Node(n)
+    nd = Node(n)
     a = random.choice(the_axes)
-    # at random offsets
-    a.add_node(n, random.random())
+    # at random offsets, add_node method calculates node's x,y
+    a.add_node(nd, random.random())
+
+    # alter node drawing after adding it to axis
+    if random.choice([True, False]):
+        nd.dwg = nd.dwg.circle(center = (nd.x, nd.y),
+                               r      = float(nx.degree(g, n)) / 20.0,
+                               fill   = 'orange',
+                               stroke = random.choice(['red','green','blue','purple']),
+                               stroke_width = 0.1)
+    else:
+        # nodes' drawings can be any svg shape
+        degree = float(nx.degree(g, n)) / 20.0
+        nd.dwg = nd.dwg.rect(insert = (nd.x - (degree/2.0), nd.y - (degree/2.0)),
+                             size   = (degree, degree),
+                             fill   = 'green',
+                             stroke = random.choice(['red','green','blue','purple']),
+                             stroke_width = 0.1)
 
 
 
@@ -33,11 +49,14 @@ for n in g.nodes():
 # edges from axis0 to axis1
 for e in g.edges():
     if (e[0] in axis0.nodes) and (e[1] in axis1.nodes):
-        h.connect(axis0, e[0], 45,
-                  axis1, e[1], -45,
+        # pass any SVG attributes to an edge
+        h.connect(axis0, e[0],
+                  45,  # source angle
+                  axis1, e[1], 
+                  -45, # target angle
                   stroke_width='0.34',
-                  stroke_opacity='0.5',
-                  stroke='green',
+                  stroke_opacity='0.4',
+                  stroke='purple',
                   fill='none')
 
 # edges from axis0 to axis2
@@ -46,7 +65,7 @@ for e in g.edges():
         h.connect(axis0, e[0], -45,
                   axis2, e[1], 45,
                   stroke_width='0.34',
-                  stroke_opacity='0.8',
+                  stroke_opacity='0.4',
                   stroke='red',
                   fill='none')
 
@@ -56,7 +75,7 @@ for e in g.edges():
         h.connect(axis1, e[0], 15,
                   axis2, e[1], -15,
                   stroke_width='0.34',
-                  stroke_opacity='0.7',
+                  stroke_opacity='0.4',
                   stroke='blue',
                   fill='none')
 
