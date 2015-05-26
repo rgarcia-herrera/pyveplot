3f38c17767a760ecd990f4890953d8d1f9fb7308
absorto <absorto@sdf.org>
2015-05-25 23:03:13 -0500

    even a small change to readme, version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
     
 setup(name='pyveplot',
-      version='0.3',
+      version='0.4',
       description='SVG Hiveplot Python API',
       long_description=readme(),
       classifiers=[
