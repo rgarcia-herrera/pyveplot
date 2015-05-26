abe8582a5b0c4bcfd20ab07f0cfad62b365a2d93
absorto <absorto@sdf.org>
2015-05-25 22:58:46 -0500

    RST makes me suffer

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
     
 setup(name='pyveplot',
-      version='0.2',
+      version='0.3',
       description='SVG Hiveplot Python API',
       long_description=readme(),
       classifiers=[
