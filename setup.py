acdf775e653c3b88bb1132f46ee6549dddb9e193
absorto <absorto@sdf.org>
2015-06-19 00:08:22 -0500

    v0.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
     
 setup(name='pyveplot',
-      version='0.5',
+      version='0.6',
       description='SVG Hiveplot Python API',
       long_description=readme(),
       classifiers=[
