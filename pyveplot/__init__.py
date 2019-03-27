ceb73bd715e9abebd37c921e938df85119cb0d63
Chris Barnes <barnesc@janelia.hhmi.org>
2019-03-27 13:54:01 -0400

    Reformat code

--- pyveplot/__init__.py
+++ pyveplot/__init__.py
@@ -17,7 +17,6 @@ from svgwrite import cm, mm
 from math import sin, cos, atan2, degrees, radians, tan, sqrt
 
 
-
 class Hiveplot:
     """
     Base class for a Hive plot.
@@ -62,16 +61,18 @@ class Hiveplot:
         h.save()
 
     """
-    def __init__( self, filename):
-        self.dwg   = svgwrite.Drawing(filename=filename, debug=True)
-        self.axes  = []
+
+    def __init__(self, filename):
+        self.dwg = svgwrite.Drawing(filename=filename, debug=True)
+        self.axes = []
 
     def draw_axes(self):
         for axis in self.axes:
             self.dwg.add(axis.getDwg())
 
-
-    def connect(self, axis0, n0_index, source_angle, axis1, n1_index, target_angle, **kwargs):
+    def connect(
+        self, axis0, n0_index, source_angle, axis1, n1_index, target_angle, **kwargs
+    ):
         """Draw edges as Bézier curves.
 
         Start and end points map to the coordinates of the given nodes
@@ -95,43 +96,38 @@ class Hiveplot:
         kwargs       : extra SVG attributes for path element, optional
                        Set or change attributes using key=value
 
-        """        
-        n0    = axis0.nodes[n0_index]
-        n1    = axis1.nodes[n1_index]
+        """
+        n0 = axis0.nodes[n0_index]
+        n1 = axis1.nodes[n1_index]
 
-        pth  = self.dwg.path(d="M %s %s" % (n0.x, n0.y), fill='none', **kwargs) # source
+        pth = self.dwg.path(d="M %s %s" % (n0.x, n0.y), fill="none", **kwargs)  # source
 
         # compute source control point
         alfa = axis0.angle() + radians(source_angle)
-        length = sqrt( ((n0.x - axis0.start[0])**2) + ((n0.y-axis0.start[1])**2)) 
-        x = axis0.start[0] + length * cos(alfa);
-        y = axis0.start[1] + length * sin(alfa);
+        length = sqrt(((n0.x - axis0.start[0]) ** 2) + ((n0.y - axis0.start[1]) ** 2))
+        x = axis0.start[0] + length * cos(alfa)
+        y = axis0.start[1] + length * sin(alfa)
 
-        pth.push("C %s %s" % (x, y)) # first control point in path
+        pth.push("C %s %s" % (x, y))  # first control point in path
 
         # compute target control point
         alfa = axis1.angle() + radians(target_angle)
-        length = sqrt( ((n1.x - axis1.start[0])**2) + ((n1.y-axis1.start[1])**2)) 
-        x = axis1.start[0] + length * cos(alfa);
-        y = axis1.start[1] + length * sin(alfa);
-        
-        pth.push("%s %s" % (x, y))   # second control point in path
+        length = sqrt(((n1.x - axis1.start[0]) ** 2) + ((n1.y - axis1.start[1]) ** 2))
+        x = axis1.start[0] + length * cos(alfa)
+        y = axis1.start[1] + length * sin(alfa)
 
-        pth.push("%s %s" % (n1.x, n1.y)) # target
+        pth.push("%s %s" % (x, y))  # second control point in path
+
+        pth.push("%s %s" % (n1.x, n1.y))  # target
         self.dwg.add(pth)
 
-            
     def save(self):
         self.draw_axes()
         self.dwg.save()
 
 
-
-
-        
 class Axis:
-    
-    def __init__( self, start=(0,0), end=(0,0), **kwargs):
+    def __init__(self, start=(0, 0), end=(0, 0), **kwargs):
         """Initialize Axis object with start, end positions and optional SVG attributes
 
         Parameters
@@ -149,11 +145,10 @@ class Axis:
         
         """
         self.start = start
-        self.end   = end
+        self.end = end
         self.nodes = {}
-        self.dwg   = svgwrite.Drawing()
+        self.dwg = svgwrite.Drawing()
         self.attrs = kwargs
-
 
     def add_node(self, node, offset):
         """Add a Node object to nodes dictionary, calculating its coordinates using offset
@@ -167,31 +162,27 @@ class Axis:
 
         """
         # calculate x,y from offset considering axis start and end points
-        width  = self.end[0] - self.start[0]
-        height = self.end[1] - self.start[1]        
+        width = self.end[0] - self.start[0]
+        height = self.end[1] - self.start[1]
         node.x = self.start[0] + (width * offset)
         node.y = self.start[1] + (height * offset)
         self.nodes[node.ID] = node
 
-        
     def draw(self):
         # draw axis
-        self.dwg.add( self.dwg.line( start = self.start,
-                                     end   = self.end,
-                                     **self.attrs ))
+        self.dwg.add(self.dwg.line(start=self.start, end=self.end, **self.attrs))
         # draw my nodes
         for node in self.nodes.values():
-            self.dwg.add( node.getDwg() )
+            self.dwg.add(node.getDwg())
 
     def getDwg(self):
         self.draw()
         return self.dwg
 
-    def angle (self):
+    def angle(self):
         xDiff = self.end[0] - self.start[0]
         yDiff = self.end[1] - self.start[1]
         return atan2(yDiff, xDiff)
-
 
 
 class Node:
@@ -201,6 +192,7 @@ class Node:
     object in the *dwg* attribute.
 
     """
+
     def __init__(self, ID):
         """
         Parameters
@@ -211,11 +203,7 @@ class Node:
         self.x = 0
         self.y = 0
         # self.r = 1.5
-        self.dwg   = svgwrite.Drawing()        
-        
+        self.dwg = svgwrite.Drawing()
+
     def getDwg(self):
         return self.dwg
-
-
-
-        
