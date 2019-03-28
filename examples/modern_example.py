1f7763458ede16a29b6dcfd824cde1b73ccaaaf1
Chris Barnes <barnesc@janelia.hhmi.org>
2019-03-28 15:47:11 -0400

    example with modern features

---
+++ examples/modern_example.py
@@ -0,0 +1,67 @@
+"""Produces a plot visually the same as short_example, but uses modern features"""
+from __future__ import absolute_import, division, unicode_literals, print_function
+import random
+from math import pi
+from pyveplot import Hiveplot
+import networkx
+
+random.seed(1)
+
+# a network
+g = networkx.barabasi_albert_graph(50, 2, seed=2)
+
+# numbers use px units
+center = (200, 200)
+# our hiveplot object
+h = Hiveplot("modern_example.svg", center=center)
+
+axis0 = h.add_axis(start=center, end=(200, 100), stroke="grey")
+# polar coordinates (radius, angle): defaults to radians
+circle = 2 * pi
+# str units are interpreted correctly
+axis1 = h.add_axis_polar(end=("105pt", circle / 3), stroke="blue")
+axis2 = h.add_axis_polar(end=("5.82cm", 240), use_radians=False, stroke="black")
+
+# randomly distribute nodes in axes
+for n in g.nodes():
+    random.choice(h.axes).add_node(n, random.random())
+
+for e in g.edges():
+    if (e[0] in axis0.nodes) and (e[1] in axis1.nodes):  # edges from axis0 to axis1
+        h.connect(
+            axis0,
+            e[0],
+            45,
+            axis1,
+            e[1],
+            -45,
+            stroke_width="0.34",
+            stroke_opacity="0.4",
+            stroke="purple",
+        )
+    elif (e[0] in axis0.nodes) and (e[1] in axis2.nodes):  # edges from axis0 to axis2
+        h.connect(
+            axis0,
+            e[0],
+            -45,
+            axis2,
+            e[1],
+            45,
+            stroke_width="0.34",
+            stroke_opacity="0.4",
+            stroke="red",
+        )
+    elif (e[0] in axis1.nodes) and (e[1] in axis2.nodes):  # edges from axis1 to axis2
+        h.connect(
+            axis1,
+            e[0],
+            15,
+            axis2,
+            e[1],
+            -15,
+            stroke_width="0.34",
+            stroke_opacity="0.4",
+            stroke="magenta",
+        )
+
+h.save()
